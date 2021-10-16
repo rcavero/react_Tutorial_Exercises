1b999a52cf1e26e1c2d349037c3e9f11ba37ed9a
rcavero <ramoncaveroaras@gmail.com>
2021-10-16 07:43:51 +0000

    All exercises done. Finished

--- .learn/resets/05.1-second-styles/app.jsx
+++ .learn/resets/05.1-second-styles/app.jsx
@@ -4,28 +4,15 @@ import PropTypes from "prop-types";
 
 const buttonStyles = {
 	//write button styles here
-	background: "yellow",
-	color: "black",
-	border: "0px",
-	padding: "5px",
-	paddingLeft: "7px",
-	paddingRight: "7px",
-	fontWeight: "bold",
+
 };
 
 const badgeStyles = {
 	//write the span styles here
-	background: "red",
-	color: "white",
-	borderRadius: "50%",
-	padding: "5px",
-	paddingTop: "2px",
-	paddingBottom: "2px",
-	margin: "3px",
-	marginTop: "0px",
+
 };
 
-const Badge = (props) => {
+const Badge = props => {
 	return (
 		<button style={buttonStyles} type="button" className="btn btn-primary">
 			{props.label}
@@ -37,7 +24,7 @@ const Badge = (props) => {
 };
 Badge.propTypes = {
 	label: PropTypes.string,
-	number: PropTypes.string,
+	number: PropTypes.string
 };
 //dont forget to change the label
 ReactDOM.render(<Badge label="Alerts" number="2" />, document.querySelector("#myDiv"));
